ab61af58e240623d353c85284bf7c7f337b54f17
Eliott <eliott_nu@mail.ru>
2021-04-18 00:18:25 +0900

    Simple level generation

--- Fibrum/Assets/Scripts/LevelGenerator.cs
+++ Fibrum/Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Transform> _segments;
+    [SerializeField] private float _minDistance;
+    [SerializeField] private Transform _player;
+
+    private void Update()
     {
-        
+        Transform lastObj = _segments[_segments.Count - 1];
+        float dis = Vector3.Distance(lastObj.position, _player.position);
+
+        if(dis < _minDistance)
+        {
+            Transform firstObj = _segments[0];
+            firstObj.position = lastObj.position;
+
+            Vector3 offset = lastObj.GetComponent<Collider>().bounds.extents + firstObj.GetComponent<Collider>().bounds.extents;
+            firstObj.position += Vector3.forward * offset.z;
+
+            _segments.Remove(firstObj);
+            _segments.Add(firstObj);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
