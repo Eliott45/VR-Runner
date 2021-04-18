c64a1baac586f650c992d34633d8cb44fd1441f5
Eliott <eliott_nu@mail.ru>
2021-04-18 20:59:08 +0900

    Removing old objects and adding fog

--- Fibrum/Assets/Scripts/ObstacleKiller.cs
+++ Fibrum/Assets/Scripts/ObstacleKiller.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ObstacleKiller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _player;
+    [SerializeField] private ObstacleSpawner _spawner;
+    [SerializeField] private float _killDistanceZ;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        List<Transform> obstacles = _spawner.spawnedObstacles;
+
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            if(_player.position.z > obstacles[i].position.z + _killDistanceZ)
+            {
+                Destroy(obstacles[i].gameObject);
+            }
+        }
     }
 }
